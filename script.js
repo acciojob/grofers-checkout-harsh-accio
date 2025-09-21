b728565b77d2da642902474ec39ee351932ee36f
harsh-accio <rajputharsh9897@gmail.com>
2025-09-21 10:42:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const tbody = document.querySelector('table');
 const getSum = () => {
 //Add your code here
-  
+	let sum=0;
+	const prices = document.querySelectorAll('.price');
+	prices.forEach(td => {
+		sum+=Number(td.textContent);
+});
+	const row = document.createElement('tr')
+	row.innerHTML=`
+	<td></td>
+	<td>${sum}</td>
+	`;
+	
+	
+   tbody.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
